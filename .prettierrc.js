2892c81cb805f42187d4ac746378c81875a60930
aymen.ganfoud <aymen.ganfoud@legalplace.fr>
2020-07-25 17:15:03 +0200

    add format code package

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports =  {
+  printWidth: 180,
+  useTabs: false,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  parser: 'typescript',
+  endOfLine: 'lf'
+}
